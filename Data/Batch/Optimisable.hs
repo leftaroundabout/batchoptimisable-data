efc0ecff8a151d4481755edd6f118e61f59536a4
Justus Sagemüller <jsag@hvl.no>
2020-07-01 18:32:52 +0200

    Skeleton project.

---
+++ Data/Batch/Optimisable.hs
@@ -0,0 +1,13 @@
+-- |
+-- Module      : Data.Batch.Optimisable
+-- Copyright   : (c) Justus Sagemüller 2020
+-- License     : GPL v3
+-- 
+-- Maintainer  : (@) jsag $ hvl.no
+-- Stability   : experimental
+-- Portability : portable
+-- 
+
+
+module Data.Batch.Optimisable where
+
