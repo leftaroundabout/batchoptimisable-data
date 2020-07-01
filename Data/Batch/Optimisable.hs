f307996a35baba7d921cb17639e48532136f452a
Justus Sagemüller <jsag@hvl.no>
2020-07-01 18:55:56 +0200

    Sketch some ideas.

--- Data/Batch/Optimisable.hs
+++ Data/Batch/Optimisable.hs
@@ -8,6 +8,25 @@
 -- Portability : portable
 -- 
 
+{-# LANGUAGE TypeFamilies    #-}
+{-# LANGUAGE TypeInType      #-}
+{-# LANGUAGE KindSignatures  #-}
 
-module Data.Batch.Optimisable where
+
+module Data.Batch.Optimisable
+   ( SystemCapabilities
+   , BatchOptimisable(..)
+   ) where
+
+import Data.Kind(Type)
+import Data.Traversable
+
+data SystemCapabilities = SystemCapabilities
+
+newtype OptimiseM a = OptimiseM { runOptimiseM :: IO a }
+
+class BatchOptimisable d where
+  type Optimised d (t :: Type->Type) :: Type
+  peekOptimised :: Traversable t => Optimised d t -> t d
+  optimiseBatch :: (Traversable t, BatchOptimisable) => t d -> Optimised d t
 
